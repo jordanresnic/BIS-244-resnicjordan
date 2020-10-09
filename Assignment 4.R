c88e65c805271a2dada2309173bfd40038c35bba
Jordan Resnic <jzr222@lehigh.edu>
2020-10-08 20:35:36 -0400

    Assignment 4

---
+++ Assignment 4.R
@@ -0,0 +1,22 @@
+getwd()
+
+twitch <- read.csv("MostPopularTwitchStreamers.csv")
+## "Us" is the csv
+twitch2 <- na.omit(twitch)
+View(twitch2)
+str(twitch2)
+twitch2$Date <- as.Date(twitch2$Date, "%m/%d/%Y")
+
+library(tidyverse)
+library(scales)
+
+p <- ggplot(data = twitch2,
+            mapping = aes(x = Date,
+                          y = Viewers,
+                          color=Streamer))
+p + geom_line(mapping = aes(group = Streamer) + 
+                scale_y_continuous(labels = unit_format(unit = "M"), scale = 1e-6)
+
+              
+              
+p              
